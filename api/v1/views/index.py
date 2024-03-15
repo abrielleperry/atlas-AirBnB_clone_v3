2f4e8ca13297faec45413ec6e1f26f343acc08f3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-15 11:25:08 -0500

    adding docstring to index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+""" retruns json response status of API """
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def status():
+    """ checks status of API """
+    return jsonify({"status":"OK"})
+
+
+if __name__ == '__main__':
+    app_views.run(debug=True, host='0.0.0.0', port='5000')
